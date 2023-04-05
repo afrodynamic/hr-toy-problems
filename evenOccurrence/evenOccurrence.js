331c46dd76d69c4ef8bd551975814705d885d444
afrodynamic <33985296+afrodynamic@users.noreply.github.com>
2023-04-05 09:53:59 -0400

    feat: complete evenOccurrences.js

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -1,6 +1,6 @@
 /*
  * Find the first item that occurs an even number of times in an array.
- * Remember to handle multiple even-occurrence items and return the first one. 
+ * Remember to handle multiple even-occurrence items and return the first one.
  * Return null if there are no even-occurrence items.
 */
 
@@ -10,6 +10,35 @@
  * console.log(onlyEven); //  4
 */
 
-var evenOccurrence = function(arr) {
-  // Your code here.
+var evenOccurrence = function (arr) {
+  var count = {};
+  var index = {};
+
+  for (var i = 0; i < arr.length; i++) {
+    var item = arr[i];
+
+    count[item] = (count[item] || 0) + 1;
+
+    if (count[item] === 1) {
+      index[item] = i;
+    }
+  }
+
+  for (var key in count) {
+    if (
+      count.hasOwnProperty(key) &&
+      count[key] % 2 === 0 &&
+      index[key] !== undefined
+    ) {
+      return arr[index[key]];
+    }
+  }
+
+  return null;
 };
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  module.exports = evenOccurrence;
+} else {
+  window.evenOccurrence = evenOccurrence;
+}
