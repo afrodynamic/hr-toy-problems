2cc2331f2b52351bf4321883044ed2cccff9cbac
afrodynamic <33985296+afrodynamic@users.noreply.github.com>
2023-04-05 09:53:59 -0400

    feat: complete evenOccurrences.js

---
+++ evenOccurrence/evenOccurrence.test.js
@@ -0,0 +1,11 @@
+const evenOccurrence = require('./evenOccurrence.js');
+
+describe.only('evenOccurrence', function () {
+  test('Should return 2 for the the array [1, 3, 3, 3, 2, 4, 4, 2, 5]', function () {
+    expect(evenOccurrence([1, 3, 3, 3, 2, 4, 4, 2, 5])).toEqual(2);
+  });
+
+  test('Should return 4 for the the array [1, 7, 2, 4, 5, 6, 8, 9, 6, 4]', function () {
+    expect(evenOccurrence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4])).toEqual(4);
+  });
+});
